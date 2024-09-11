79a4bc78de59bc07f38561cec2bbdfda1a719992
muktadir1280 <muktadir1280@gmail.com>
2024-09-11 16:57:44 +0600

    problemTwo solved

---
+++ problemTwo.js
@@ -0,0 +1,10 @@
+function sendNotification(email) {
+    if (email.indexOf('@') === -1) {
+        return "Invalid Email"
+    }
+    const check = email.split('@');
+    const userName = check[0];
+    const domainName = check[1];
+    return `${userName} sent you an email from ${domainName}`;
+}
+console.log(sendNotification('sadia8icloud.com')); // remove this line
